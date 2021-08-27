f8416ef2e98b09ca5e0e2556b76b8f8339057776
thaitv10 <thaitv210@gmail.com>
2021-08-27 16:16:43 +0700

    rename id

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ ReactDOM.render(
   <React.StrictMode>
     <ChatUI />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('chatbot')
 );
 
 // If you want to start measuring performance in your app, pass a function
